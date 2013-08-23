0095fb561c9d851b5de45851fc5e84384418826c
Fabian Grünbichler <fabian.gruenbichler@tuwien.ac.at>
2013-08-24 00:18:35 +0200

    fix indentation

--- liboath/errors.c
+++ liboath/errors.c
@@ -58,7 +58,8 @@ static const err_t errors[] = {
   ERR (OATH_FILE_SYNC_ERROR, "System error when syncing file to disk"),
   ERR (OATH_FILE_CLOSE_ERROR, "System error when closing file"),
   ERR (OATH_SUITE_PARSE_ERROR, "Error while parsing OCRA suite string"),
-  ERR (OATH_SUITE_MISMATCH_ERROR, "Mismatch between OCRA suite parameters and data input")
+  ERR (OATH_SUITE_MISMATCH_ERROR,
+       "Mismatch between OCRA suite parameters and data input")
 };
 
 /**
